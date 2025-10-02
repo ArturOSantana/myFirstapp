23ac5237641b86a9c0793cf0fc3ce11f6b9d4891
Artur O. de Santana <artur.santana.dev@gmail.com>
2025-10-02 17:30:28 -0300

    Ainda tentando

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,26 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Double } from "react-native/Libraries/Types/CodegenTypes";
 
 export default function Home(){
  
     const [name, setName ] = useState('');
     const [idade, setIdade] = useState('');
 
+    let [p,setP] = useState<number>(0);
+    let [a,setA] = useState<number>(0);
+
+   
+
+    function calcularImc(p:Double, a:Double){
+      
+      p / (a*a);
+
+
+        return p / (a*a);
+    }
+
+    let imc:Double = calcularImc(p,a);
 
     return(
         <View style ={styles.body}> 
@@ -31,6 +46,28 @@ export default function Home(){
                 Olá {name}, você tem {idade}
             </Text>
 
+            <Text style = {styles.title}>
+                Digite seu Peso: 
+            </Text>
+            <TextInput 
+          placeholder="PESO"
+          onChangeText = {setP}/>
+
+             <Text style = {styles.title}>
+                Digite sua Altura: 
+            </Text>
+            <TextInput 
+            placeholder="1.70"
+            onChange = {setA}/>
+
+            <Text>
+                O seu IMC é {imc}
+
+            </Text>
+            
+
+            
+        
 
         </View>
     ); 
