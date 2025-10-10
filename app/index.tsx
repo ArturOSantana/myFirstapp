f3cde9a9d71ee9fcca047d400d9e8298f91d4565
Artur O. de Santana <artur.santana.dev@gmail.com>
2025-10-09 21:41:21 -0300

    Ultimas atualizações

--- app/index.tsx
+++ app/index.tsx
@@ -1,86 +1,31 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
-import { Double } from "react-native/Libraries/Types/CodegenTypes";
+import { fetchCharacters } from "@/services/list";
+import React, { useEffect, useState } from "react";
+import { FlatList } from "react-native-reanimated/lib/typescript/Animated";
 
 export default function Home(){
- 
-    const [name, setName ] = useState('');
-    const [idade, setIdade] = useState('');
+    const [personagens,setPersonagens] = useState([]);
 
-    let [p,setP] = useState<number>(0);
-    let [a,setA] = useState<number>(0);
-
-   
-
-    function calcularImc(p:Double, a:Double){
-      
-      p / (a*a);
-
-
-        return p / (a*a);
-    }
-
-    let imc:Double = calcularImc(p,a);
-
+    useEffect(() => {
+        async function carregarPersonagens(){
+            const dados = await fetchCharacters();
+            setPersonagens(dados.characters);
+        }
+    },[]);
     return(
-        <View style ={styles.body}> 
+        <view>
+            <text> Lista de Personagens</text>
+            <FlatList
+                data={personagens}
+                keyExtractor={item => item.id.toString()}
+                renderItem={({item})} => (
+                    <View>
+                        <Image source={{uri:item[0]}}/>
+                        
 
-        <Text style = {styles.title}> 
-            Digite seu nome
-        </Text>
-        <TextInput 
-            placeholder="Digite seu nome"
-            onChangeText={setName}/> 
-            
-          
-            <Text style = {styles.title}>
-                Olá {name}, digite sua idade:
-            </Text>
-              <TextInput
-                placeholder="Digite sua idade"
-                onChangeText={setIdade}
-            /> 
-
-            <Text style = {styles.title}
-            >
-                Olá {name}, você tem {idade}
-            </Text>
-
-            <Text style = {styles.title}>
-                Digite seu Peso: 
-            </Text>
-            <TextInput 
-          placeholder="PESO"></TextInput>
-          
-
-             <Text style = {styles.title}>
-                Digite sua Altura: 
-            </Text>
-            <TextInput 
-            placeholder="1.70"/>
-
-            <Text>
-                O seu IMC é {imc}
-
-            </Text>
-            
-
-            
-        
-
-        </View>
-    ); 
-}
-const styles = StyleSheet.create({
-    body: {
-        backgroundColor: "blue",
-        padding: "auto"
-    }, 
-    title: {
-        fontSize: 34,
-        fontFamily:"arial",
-        color:"white"
-    }
+                    </View>
+                )
+            />
+        </view>
+    )
     
-
-})
+}
